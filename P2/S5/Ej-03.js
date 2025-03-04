ed52c291b4a660f878ef9615f65dc8af10a2174e
jorge <j.martinezg.2022@alumnos.urjc.es>
2025-03-04 11:24:12 +0100

    S5

---
+++ P2/S5/Ej-03.js
@@ -0,0 +1,25 @@
+console.log("Ejecutando JS...");
+
+//-- Crear objeto gui, con los elementos de la interfaz gráfica
+//-- Al tenerlo agrupado podemos pasarlo como parámetro o asignarlo
+//-- a otro objeto
+const gui = {
+  display: document.getElementById("display"),
+  boton: document.getElementById("boton"),
+}
+
+const value = 8;
+
+//-- Objeto contador: Contiene el valor y el método para incrementarse
+const counter = {
+  valor: 1,
+  inc : function(value) {
+    this.valor *= value;
+    gui.display.innerHTML = this.valor;
+  }
+}
+
+//-- Acciones: Ligar el botón al contador
+gui.boton.onclick = () => {
+  counter.inc(value)
+}
